2600b3806a8fe00383d993486c2b321e457e480c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-11-23 16:22:46 +0000

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,10 @@
+import math
+print("\033c\033[43;30m\ncube volume?w=")
+w=float(input())
+print("spere volume?h=")
+h=float(input())
+print("spere volume?z=")
+z=float(input())
+
+volume = w*h*z
+print(volume)
